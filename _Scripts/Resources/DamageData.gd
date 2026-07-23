519335b9691054e5d76b484847d09d56f450ce64
mydoot <buaqur@gmail.com>
2026-07-23 01:16:00 -0700

    added bullet collision, bullet can kill enemies, basic LOS implementation

---
+++ _Scripts/Resources/DamageData.gd
@@ -0,0 +1,13 @@
+class_name DamageData extends Resource
+
+var damage: int
+
+# Enemies should have this variable be false.
+var is_from_player: bool
+
+@export_group("Bullet Type Booleans")
+var is_ricoshot: bool
+
+var is_piercing: bool
+
+# Feel free to add more booleans to determine variables, or not even use this idk lol
